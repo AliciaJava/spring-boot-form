37486757d50cef8c3cedeb52e86a33f651a13e19
Alicia <aliciaortega1986@gmail.com>
2025-02-15 21:39:03 +0100

    añadir anotaciones lombok

--- src/main/java/com/springboot/form/app/models/domain/Role.java
+++ src/main/java/com/springboot/form/app/models/domain/Role.java
@@ -1,43 +1,20 @@
 package com.springboot.form.app.models.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 
 	private Integer id;
 	private String nombre;
 	private String role;
 
-	public Role() {
-	}
-
-	public Role(Integer id, String nombre, String role) {
-		this.id = id;
-		this.nombre = nombre;
-		this.role = role;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
 
 	@Override
 	public boolean equals(Object obj) {
